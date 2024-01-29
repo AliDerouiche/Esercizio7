437c377d4486671c3497a9f6637697c9b172780d
Bisco <alidero1605@gmail.com>
2024-01-29 12:48:20 +0100

    Esercizio4 Ereditarietà - 2

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle extends Shape {
+
+    public Rectangle(double base, double height) {
+        super(base, height);
+    }
+
+    @Override
+    public double areaCalculator() {
+        double area = base * height;
+
+        return area;
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,15 @@
+public class Shape {
+    double base;
+    double height;
+
+    public Shape(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double areaCalculator() {
+        double area = (base * height) / 2;
+        return area;
+    }
+
+}
